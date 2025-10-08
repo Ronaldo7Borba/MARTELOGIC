77376e76a880d26d1387e6823fd49b83504bb858
Ronaldo <rborba00.rb@gmail.com>
2025-10-08 20:18:21 -0300

    atualização página de login

--- backe.js
+++ backe.js
@@ -8,6 +8,6 @@ app.get('/usuario', (req, res) =>{
 });
 
 app.listen(3000, () => {
-    console.log('Servidor rodando na porta http://localhost:3000');
+    console.log('Servidor rodando na porta http://localhost:27017');
 });
 
